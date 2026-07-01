00b2256217be6a4d47088f10ed3132e1bc0ffa56
André <gwandrewgd@gmail.com>
2026-07-01 18:54:26 +0200

    Added message structure.

--- incl/protocol.h
+++ incl/protocol.h
@@ -1,3 +1,29 @@
-/* file    : incl/protocol.h
-   owner   : shared
-   role    : define shared packet structures and command types */
+#include <stdint.h>
+
+typedef struct s_dm_msg
+{
+   uint8_t        msg_type;
+   uint64_t       msg_id;
+   uint64_t       sender_id;
+   uint64_t       reciever_id;
+   char           *content;
+
+} t_dm_msg;
+
+enum MESSAGE_TYPE
+{
+   TEXT,
+   REACTION,
+};
+
+enum MESSAGE_LOCATION
+{
+   SERVER,
+   DM,
+};
+
+enum SERVER_TYPE
+{
+   PUBLIC,
+   PRIVATE
+};
--- src/main.c
+++ src/main.c
@@ -1,30 +1,5 @@
-#include <gtk/gtk.h>
-#define W 1280
-#define H 720
-#include "app.h"
-
-static void on_activate(GtkApplication *app, gpointer user_data)
-{
-    (void)user_data;
-
-    GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "myDiscord");
-    gtk_window_set_default_size(GTK_WINDOW(window), W, H);
-
-    GtkWidget *label = gtk_label_new(app_greeting());
-    gtk_window_set_child(GTK_WINDOW(window), label);
-
-    gtk_window_present(GTK_WINDOW(window));
-}
 
 int main(int argc, char *argv[])
 {
-    GtkApplication *app = gtk_application_new("com.example.myDiscord",
-                                              G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-
-    int status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
-
-    return status;
+    return 0;
 }
